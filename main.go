bcaacdc4b205dc3cc78c55820744dede04800e13
Nevezhin Yan <yaniknezhin@gmail.com>
2023-10-12 16:20:30 +0600

    Accepting a single line at /snippet/view

--- main.go
+++ main.go
@@ -1,31 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	w.Write([]byte("Hello from home!"))
+	w.Header().Set("Allow", http.MethodPost)
+	http.Error(w, "The method does nothing", http.StatusMethodNotAllowed)
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from snippetView!"))
-}
-
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from snippetCreate!"))
+	// Extract the value of the id parameter from the query string and try to
+	// convert it to an integer using the strconv.Atoi() function. If it can't
+	// be converted to an integer, or the value is less than 1, we return a 404 page
+	// not found response.
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	// Use the fmt.Fprintf() function to interpolate the id value with our response
+	// and write it to the http.ResponseWriter.
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func main() {
-	http.HandleFunc("/", home)
-	http.HandleFunc("/snippet/view", snippetView)
-	http.HandleFunc("/snippet/create", snippetCreate)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
 	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", nil)
+	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
